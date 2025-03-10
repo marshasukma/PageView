7a5e9a89f4961345386524938484619152393774
Marsha Sukma <marsukman23@gmail.com>
2025-03-10 07:21:36 +0700

    Tugas 3.1

---
+++ tutorial_11-1/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+void main() {
+  runApp(const MyApp11_1());
+}
+
+class MyApp11_1 extends StatelessWidget {
+  const MyApp11_1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ABP Minggu 11',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ABP Minggu 11'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selected = 0;
+  PageController pc = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: PageView(
+          controller: pc,
+          onPageChanged: (index) {
+            setState(() {
+              selected = index;
+            });
+          },
+          children: [
+            Center(
+              child: InkWell(
+                child: Text('Go to Home page', style: TextStyle(fontSize: 30)),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MyApp11_1(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: Text('Email page', style: TextStyle(fontSize: 30)),
+            ),
+            Center(
+              child: Text('Profile page', style: TextStyle(fontSize: 30)),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: const Color.fromARGB(255, 11, 19, 66),
+        unselectedItemColor: Colors.white,
+        currentIndex: selected,
+        onTap: (index) {
+          setState(() {
+            selected = index;
+          });
+          pc.animateToPage(
+            index,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.linear,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.email),
+            label: 'Email',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
